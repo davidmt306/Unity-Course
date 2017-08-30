b42ac170a00c0ef8e1c4e013b696bf90447a834d
David Martínez <davidmt_306@hotmail.com>
2017-08-29 20:24:58 -0500

    Ship moving & Camera edges
    - Camera.ViewportToWorldPoint()
    - Restricted movement to camera edges

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15.0f;
+	public float padding = 1.0f;
+	
+	float xMin;
+	float xMax;
+
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distance));
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			//transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			//transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		// Restrict the player to the game space
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
