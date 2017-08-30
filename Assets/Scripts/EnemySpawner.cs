3d72beaa6c60c83d7eb110f84ef2d2a425cae0d7
David Martínez <davidmt_306@hotmail.com>
2017-08-29 21:08:48 -0500

    Spawning enemies
    - Spawn enemies on positions.
    - Draw circle on Game Objects (position)
    - For each to create Enemy clones

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	
+	public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform) {
+			// Spawn an enemy
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			// Put the created enemies under Enemy Formation Game Object
+			enemy.transform.parent = child;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,7 +5,6 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		//Debug.Log ("Level load requested for: " + name);
-		Brick.breakableCount = 0;
 		Application.LoadLevel(name);
 	}
 	
@@ -15,13 +14,6 @@ public class LevelManager : MonoBehaviour {
 	}
 	
 	public void LoadNextLevel () {
-		Brick.breakableCount = 0;
 		Application.LoadLevel(Application.loadedLevel + 1);
-	}
-	
-	public void BrickDestroyed () {
-		if (Brick.breakableCount <= 0) {
-			LoadNextLevel();
-		}
 	}
 }
