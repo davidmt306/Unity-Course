e7be183e4b393381fa8660297487f35e3eaae088
davidmt306 <davidmt_306@hotmail.com>
2018-03-14 20:49:14 -0600

    Making a Splash Screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	// Carga el siguiente nivel de manera automatica
+	void Start () {
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name) {
+		//Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("I want to quit");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel () {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
